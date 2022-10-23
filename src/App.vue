e73b0f0509a8ea302789a0e82b239629fc5fe5f3
ImT3chy <mads4295kk@gmail.com>
2022-10-23 11:12:07 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import './index.css';
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+
+const loggedIn = false;
+
+
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+  </header>
+
+  <RouterView />
+</template>
